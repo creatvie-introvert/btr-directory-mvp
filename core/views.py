035df13226c237f143e1ea5968c6bb2426e7fc4d
urbanIntrovert <trefoil72@gmail.com>
2025-12-31 15:24:48 +0000

    feat: add base layout with navbar, footer, and hero

--- core/views.py
+++ core/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+
 # Create your views here.
 def home(request):
-    return HttpResponse("Core app is wired up ✅")
+    return render(request, "core/home.html")
