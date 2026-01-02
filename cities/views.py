2bce6e8fd303e05e1e4f89029bf42dbd44e582c7
urbanIntrovert <trefoil72@gmail.com>
2026-01-02 12:37:51 +0000

    feat(cities): add citties list page and wire navigation

--- cities/views.py
+++ cities/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from cities.models import City
 
 
 # Create your views here.
-
+def city_list(request):
+    cities = City.objects.filter(is_active=True).order_by("name")
+    return render(request, "cities/city_list.html", {"cities": cities})
--- config/urls.py
+++ config/urls.py
@@ -8,7 +8,7 @@ Function views
     1. Add an import:  from my_app import views
     2. Add a URL to urlpatterns:  path('', views.home, name='home')
 Class-based views
-    1. Add an import:  from other_app.views import Home
+    1. Add a, n import:  from other_app.views import Home
     2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
 Including another URLconf
     1. Import the include() function: from django.urls import include, path
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('core.urls')),
+    path('cities/', include('cities.urls')),
 ]
